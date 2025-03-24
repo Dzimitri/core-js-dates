424bf82c170c3ac0a2a9907436688601fea5fd25
Dzmitry <d.prankevich@gmail.com>
2025-03-24 20:23:32 +0300

    feat: add solution 10th task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -208,8 +208,13 @@ function getCountWeekendsInMonth(month, year) {
  * Date(2024, 0, 31) => 5
  * Date(2024, 1, 23) => 8
  */
-function getWeekNumberByDate(/* date */) {
-  throw new Error('Not implemented');
+function getWeekNumberByDate(date) {
+  const copyDate = new Date(date);
+  copyDate.setHours(0, 0, 0, 0);
+  copyDate.setDate(copyDate.getDate() + 3 - ((copyDate.getDay() + 6) % 7));
+
+  const yearStart = new Date(copyDate.getFullYear(), 0, 1);
+  return Math.ceil(((copyDate - yearStart) / 86400000 + 1) / 7);
 }
 
 /**
