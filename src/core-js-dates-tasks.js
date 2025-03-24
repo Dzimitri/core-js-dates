de3c199ce3283f5f886b8253741e27dc432b1894
Dzmitry <d.prankevich@gmail.com>
2025-03-24 20:03:11 +0300

    feat: add solution 9th task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -182,8 +182,17 @@ function formatDate(date) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  let count = 0;
+  const daysMounth = new Date(year, month, 0).getDate();
+
+  for (let day = 1; day <= daysMounth; day += 1) {
+    const currentDate = new Date(year, month - 1, day);
+    if (currentDate.getDay() === 0 || currentDate.getDay() === 6) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 /**
